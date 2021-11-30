53b102cb1035b350301eb187890fab0a6c8a2c5d
davidorellana <davidorellana_98@hotmail.com>
2021-11-30 15:20:19 -0500

    Inicialización del proyecto

---
+++ app/src/main/java/com/davidorellana/splashscreenconjetpackcompose/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.davidorellana.splashscreenconjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(color = MaterialTheme.colors.background) {
+
+            }
+        }
+    }
+}
