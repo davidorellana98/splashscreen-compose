b91ea6e9ad424f752f35d3a1e56084a53c42f7ae
davidorellana <davidorellana_98@hotmail.com>
2021-11-30 21:04:31 -0500

    Implementación para ejecución de la app

--- app/src/main/java/com/davidorellana/splashscreenconjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/davidorellana/splashscreenconjetpackcompose/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Surface(color = MaterialTheme.colors.background) {
-
+                NavigationScreen()
             }
         }
     }
