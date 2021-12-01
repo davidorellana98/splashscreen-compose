ceecb6fb62dabee0b98f69db1eb5f56ee3ed01af
davidorellana <davidorellana_98@hotmail.com>
2021-11-30 21:01:48 -0500

    Creación de la pantalla Splash Screen

---
+++ app/src/main/java/com/davidorellana/splashscreenconjetpackcompose/SplashScreen.kt
@@ -0,0 +1,116 @@
+package com.davidorellana.splashscreenconjetpackcompose
+
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationVector1D
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+
+    val scaleAnimation: Animatable<Float, AnimationVector1D> = remember { Animatable(initialValue = 0f) }
+
+    AnimationSplashContent(
+        scaleAnimation = scaleAnimation,
+        navController = navController,
+        durationMillisAnimation = 1500,
+        delayScreen = 3000L
+    )
+
+    DesignSplashScreen(
+        imagePainter = painterResource(id = R.drawable.ic_logo_kotlin),
+        scaleAnimation = scaleAnimation
+    )
+}
+
+@Composable
+fun AnimationSplashContent(
+    scaleAnimation: Animatable<Float, AnimationVector1D>,
+    navController: NavController,
+    durationMillisAnimation: Int,
+    delayScreen: Long
+) {
+
+    LaunchedEffect(key1 = true) {
+        scaleAnimation.animateTo(
+            targetValue = 0.5F,
+            animationSpec = tween(
+                durationMillis = durationMillisAnimation,
+                easing = {
+                    OvershootInterpolator(3F).getInterpolation(it)
+                }
+            )
+        )
+
+        delay(timeMillis = delayScreen)
+
+        navController.navigate(route = DestinationScreen.MainScreenDest.route) {
+            popUpTo(route = DestinationScreen.SplashScreenDest.route) {
+                inclusive = true
+            }
+        }
+    }
+}
+
+@Composable
+fun DesignSplashScreen(
+    modifier: Modifier = Modifier,
+    imagePainter: Painter,
+    scaleAnimation: Animatable<Float, AnimationVector1D>
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(
+                        Color(239, 127, 26),
+                        Color(120, 40, 140),
+                        Color(0, 47, 187),
+                    )
+                )
+            ),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = imagePainter,
+                contentDescription = "Logotipo Splash Screen",
+                modifier = modifier
+                    .size(400.dp)
+                    .scale(scale = scaleAnimation.value),
+            )
+
+            Text(
+                text = "Aquí va el logotipo de su preferencia",
+                color = Color.White,
+                fontSize = 40.sp,
+                fontWeight = FontWeight.ExtraBold,
+                fontFamily = FontFamily.Serif,
+                textAlign = TextAlign.Center,
+                modifier = modifier.scale(scale = scaleAnimation.value)
+            )
+        }
+    }
+}
